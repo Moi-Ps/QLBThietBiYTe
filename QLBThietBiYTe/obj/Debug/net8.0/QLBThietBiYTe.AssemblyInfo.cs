2c9cbff6eba0ddaa2ab4330e5ea5f009e31a114a
Moi-Ps <moips103@gmail.com>
2025-02-21 21:42:35 +0700

    tabs

--- QLBThietBiYTe/obj/Debug/net8.0/QLBThietBiYTe.AssemblyInfo.cs
+++ QLBThietBiYTe/obj/Debug/net8.0/QLBThietBiYTe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLBThietBiYTe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05719cf3a1e4643e4e21e3a94b3a697e786dffda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050252459735f14df6fa2851684aabd510f9f62a")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLBThietBiYTe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLBThietBiYTe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
